6e832504855365c3968431d6a22fc2adb7f1b811
zzccchen <2923400952@qq.com>
2018-11-26 23:06:15 +0800

    添加滚动条字幕

--- src/components/RefreshData.vue
+++ src/components/RefreshData.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- circle 组件 -->
     <div class="vux-circle-demo">
       <div class="vux-circle-demo-p">
         <x-circle :percent="percent"
@@ -7,21 +8,26 @@
           :trail-width="1"
           :stroke-color="['#36D1DC', '#5B86E5']"
           trail-color="#ececec">
+          <!-- circle 内含 -->
+          <!-- 百分数 -->
           <countup v-if="step<3"
             :start-val="1"
             :end-val="percent"
             :duration="3"
             class="vux-circle-demo-font"></countup>
+          <!-- 百分号 -->
           <span v-if="step<3"
             class="vux-circle-demo-font">%</span>
+          <!-- 成功弯勾 -->
           <svg v-else
             class="icon myIconStyle"
             aria-hidden="true">
-            　　<use xlink:href="#icon-dagouwuquan"></use>
+            <use xlink:href="#icon-dagouwuquan"></use>
           </svg>
         </x-circle>
       </div>
     </div>
+    <!-- marquee 组件 -->
     <div class="vux-marquee">
       <marquee :interval=3000>
         <marquee-item v-for="i in marqueeMsg"
@@ -30,28 +36,27 @@
           style="list-style:none;">{{i}}</marquee-item>
       </marquee>
     </div>
-    <div>
-      <box class="vux-button">
-        <x-button @click.native="stepPlus"
-          v-if="step==0"
-          :gradients="['#1D62F0', '#19D5FD']"
-          style="border-radius:99px;">同 步 数 据</x-button>
-        <x-button @click.native="stepPlus"
-          v-else-if="step==1"
-          :gradients="['#6F1BFE', '#9479DF']"
-          style="border-radius:99px;"
-          show-loading>爬 取 数 据 中 ...</x-button>
-        <x-button @click.native="stepPlus"
-          v-else-if="step==2"
-          :gradients="['#A644FF', '#FC5BC4']"
-          style="border-radius:99px;"
-          show-loading>数 据 同 步 中 ...</x-button>
-        <x-button v-else
-          plain
-          disabled
-          style="border-radius:99px;color: #97A8B0;">同 步 数 据 (21min)</x-button>
-      </box>
-    </div>
+    <!-- button 组件 -->
+    <box class="vux-button">
+      <x-button @click.native="stepPlus"
+        v-if="step==0"
+        :gradients="['#1D62F0', '#19D5FD']"
+        style="border-radius:99px;">同 步 数 据</x-button>
+      <x-button @click.native="stepPlus"
+        v-else-if="step==1"
+        :gradients="['#6F1BFE', '#9479DF']"
+        style="border-radius:99px;"
+        show-loading>爬 取 数 据 中 ...</x-button>
+      <x-button @click.native="stepPlus"
+        v-else-if="step==2"
+        :gradients="['#A644FF', '#FC5BC4']"
+        style="border-radius:99px;"
+        show-loading>数 据 同 步 中 ...</x-button>
+      <x-button v-else
+        plain
+        disabled
+        style="border-radius:99px;color: #97A8B0;">同 步 数 据 (21min)</x-button>
+    </box>
   </div>
 </template>
 
@@ -73,22 +78,37 @@ export default {
     return {
       lastRefreshTime: '2018-11-25 20:00',
       percent: 0,
-      marqueeMsg: [
+      marqueeMsg0: [
+        'Tip: 每 40 分钟才可同步一次哦～',
+        '动动手指，获取最新跑操数据吧',
+        'Tip: 只有连接 seu-wlan 才可进行同步操作',
+        '我为人人，人人为我～'
+      ],
+      marqueeMsg1: [
         '稍等片刻，马上就好',
         'Tip: 每 40 分钟才可同步一次哦～',
         '秋意正好，不如喝杯热水休息一下',
         'Tip: 只有连接 seu-wlan 才可进行同步操作',
-        '我为人人，人人为我～'
+        '微微正在努力同步中～',
+        'Tip: 每次同步耗时约为一分钟哦',
+        '关注小微 GitHub，一起参与开发～',
+        'Tip: 如有问题，欢迎致信微微邮箱'
       ],
+      marqueeMsg: null,
       step: 0
     }
+  },
+  created: function () {
+    this.marqueeMsg = this.marqueeMsg0
   },
   methods: {
     stepPlus () {
       this.step += 1
       this.percentPlus()
+      this.marqueeMsg = this.marqueeMsg1
       if (this.step === 3) {
         this.percent = 200
+        this.marqueeMsg = this.marqueeMsg0
       }
     },
     percentPlus () {
