c636c2e9b219fa077a9f40f29e9205a7281ea8c3
zzccchen <2923400952@qq.com>
2018-12-03 19:55:54 +0800

    完成主体逻辑，完善提示

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,13 +11,17 @@
       <confirm v-model="confirmShow"
         show-input
         placeholder="输入一卡通号"
-        close-on-confirm
         :title="'用户绑定'"
-        :input-attrs="{type:'number'}"
-        @on-confirm="onConfirm5"
+        :input-attrs="{type: 'number'}"
+        @on-confirm="onConfirm"
+        :close-on-confirm=false
         confirm-text="我很确定"
         :show-cancel-button=false>
       </confirm>
+      <toast v-model="toastShow2" 
+        position="bottom" 
+        type="text"
+        width="10em">请输入正确一卡通号</toast>
     </div>
     <!-- html 背景 -->
     <div class="vux-background"></div>
@@ -39,52 +43,62 @@ export default {
     Toast,
     TransferDom
   },
-  created: async function () {
-    // if (this.$cookies.get('__qc__k') == null) {
-    // //   eslint-disable-next-line
-    //   QC.Login.showPopup({
-    //     appId: '101529980',
-    //     redirectURI: 'https://seuxw.cn/webapp/paocao'
-    //   })
-    // } else
-    // if (this.$cookies.get('xAuth') == null) {
-    //   this.loadShow = false
-    //   // TODO： 添加一卡通输入框
-
-    //   let xAuth = await this.getXAuth()
-    //   if (xAuth == null) {
-    //     this.loadShow = false
-    //     this.toastShow = true
-    //   } else {
-    //     this.$cookies.set('xAuth', xAuth)
-    //     this.$router.push({
-    //       path: '/paocao',
-    //       name: 'paocao'
-    //     })
-    //   }
-    // } else {
-    //   this.loadShow = false
-    //   this.toastShow = true
-    // }
-    this.loadShow = false
+  created: function () {
+    if (this.$cookies.get('__qc__k') == null) {
+      //   eslint-disable-next-line
+      QC.Login.showPopup({
+        appId: '101529980',
+        redirectURI: 'https://seuxw.cn/webapp/paocao'
+      })
+    } else if (this.$cookies.get('xAuth') == null) {
+      this.loadShow = false
+      this.confirmShow = true
+    } else {
+      // TODO: 添加 xAuth 更新代码
+      this.$router.push({
+        path: '/paocao',
+        name: 'paocao'
+      })
+    }
   },
   data () {
     return {
       loadShow: true,
       toastShow: false,
+      toastShow2: false,
       cardno: null,
-      confirmShow: true
+      confirmShow: false
     }
   },
   methods: {
     async getXAuth (tk, cardno) {
-      let i = 5
+      let i = 3
+      // TODO: 修复 xAuth 多次请求问题
       do {
         var res = await axios.post('/xAuth?tk=' + this.$cookies.get('__qc__k') +
           '&cardno=' + this.cardno)
         res = res.data['data']['token']
       } while (--i || res == null)
       return { res: '555' }
+    },
+    async onConfirm (inputCardno) {
+      this.cardno = inputCardno
+      while (this.cardno < 213150000 || this.cardno > 213190000) {
+        this.toastShow2 = true
+        return
+      }
+      let xAuth = await this.getXAuth()
+      // TODO: 添加 xAuth 正确性判断
+      if (xAuth == null) {
+        this.toastShow = true
+      } else {
+        this.confirmShow = false
+        this.$cookies.set('xAuth', xAuth)
+        this.$router.push({
+          path: '/paocao',
+          name: 'paocao'
+        })
+      }
     }
   }
 }
